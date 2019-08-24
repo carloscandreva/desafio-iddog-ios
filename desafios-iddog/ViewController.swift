d065d86389224967269e8c1802eb2edfa5d3c62d
Carlos Candreva <carlosmc@ciandt.com>
2019-08-24 18:06:17 -0300

    Project Init

---
+++ desafios-iddog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  desafios-iddog
+//
+//  Created by Carlos Alberto Mota Candreva on 24/08/19.
+//  Copyright © 2019 Carlos Alberto Mota Candreva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
