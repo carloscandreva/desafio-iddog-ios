a6700ceaeb9e0bb2dac6c6009c4fe968a8ffea8a
Carlos Alberto Mota Candreva <carloscandreva@yahoo.com.br>
2019-08-28 01:25:51 -0300

    Dog Category Selector Screen

--- desafios-iddog/Source/ViewModels/DogsSelectorViewModel.swift
+++ desafios-iddog/Source/ViewModels/DogsSelectorViewModel.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+import Alamofire
+import UIKit
+
+protocol DogsSelectorViewModel {
+    var coordinator: MainCoordinator? {get set}
+    var token: String? {get set}
+    func showDogByCategory(category: String);
+}
+
+class DogsSelectorViewModelable: NSObject, DogsSelectorViewModel {
+    var token: String?
+    var coordinator: MainCoordinator?
+    
+    func showDogByCategory(category: String) {
+        guard let t = self.token else { return }
+        self.coordinator?.dogsGallery(token: t, category: category)
+    }
+}
