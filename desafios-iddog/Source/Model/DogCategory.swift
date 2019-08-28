5a555bc299f206b182bdda6c88ed45cf8101b13a
Carlos Alberto Mota Candreva <carloscandreva@yahoo.com.br>
2019-08-28 01:26:20 -0300

    Dog Category

---
+++ desafios-iddog/Source/Model/DogCategory.swift
@@ -0,0 +1,15 @@
+//
+//  DogsCategory.swift
+//  desafios-iddog
+//
+//  Created by Carlos Alberto Mota Candreva on 26/08/19.
+//  Copyright © 2019 Carlos Alberto Mota Candreva. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Dog
+struct DogsCategory: Codable {
+    let category: String
+    let list: [String]
+}
