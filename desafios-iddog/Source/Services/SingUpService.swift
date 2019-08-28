b46964b46fd6f8054995c1763c805c54f0d42ee8
Carlos Alberto Mota Candreva <carloscandreva@yahoo.com.br>
2019-08-28 01:24:45 -0300

    Service To Get Token From User Email

--- desafios-iddog/Source/Services/SingUpService.swift
+++ desafios-iddog/Source/Services/SingUpService.swift
@@ -7,3 +7,54 @@
 //
 
 import Foundation
+import Alamofire
+import Reachability
+
+
+/// Beer service
+protocol SingUpServiceProtocol {
+
+    /// Gets beer list
+    ///
+    /// - Parameters:
+    ///   - email: User Email For Api
+    ///   - completion: Completion block
+    func getToken(email: String, completion: @escaping ((() throws -> (String)) -> Void))
+}
+
+
+class SingUpService: SingUpServiceProtocol {
+
+    private let reachability: ChallengeReachability
+
+    init(reachability: ChallengeReachability = Reachability(hostName: ApiRouter.Constants.baseURLPath)) {
+        self.reachability = reachability
+    }
+
+    func getToken(email: String, completion: @escaping ((() throws -> (String)) -> Void)) {
+        guard reachability.internetIsReachable else {
+            completion { throw Errors.offlineMode }
+            return
+        }
+
+        Alamofire.request(ApiRouter.signUp(email))
+            .validate(statusCode: 200..<300)
+            .responseJSON { (response: DataResponse) in
+                switch response.result {
+                case .success( _):
+                    do {
+                        guard let d = response.data else { return }
+                        let decoder = JSONDecoder()
+                        let model = try decoder.decode(User.self, from: d)
+                        completion { model.user.token }
+                    } catch {
+                        completion { throw Errors.parse(String(describing: User.self)) }
+                    }
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    completion { throw error }
+                }
+        }
+
+    }
+}
