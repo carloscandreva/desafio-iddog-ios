4858eb577680c1351ac2aec2e0a7f978805fcaf0
Carlos Alberto Mota Candreva <carloscandreva@yahoo.com.br>
2019-08-28 01:28:19 -0300

    Helper Extensions

--- desafios-iddog/Source/Common/Extensions/UIColor+Extensions.swift
+++ desafios-iddog/Source/Common/Extensions/UIColor+Extensions.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+import UIKit
+
+// MARK: - Collor palette
+extension UIColor {
+    static var metal: UIColor {
+        return UIColor(displayP3Red: 43.0/255.0, green: 48.0/255.0, blue: 58.0/255.0, alpha: 1)
+    }
+
+    static var blackCoral: UIColor {
+        return UIColor(displayP3Red: 79.0/255.0, green: 93.0/255.0, blue: 117.0/255.0, alpha: 1)
+    }
+
+    static var silver: UIColor {
+        return UIColor(displayP3Red: 191.0/255.0, green: 192.0/255.0, blue: 192.0/255.0, alpha: 1)
+    }
+
+    static var lightRed: UIColor {
+        return UIColor(displayP3Red: 239.0/255.0, green: 131.0/255.0, blue: 84.0/255.0, alpha: 1)
+    }
+}
+
