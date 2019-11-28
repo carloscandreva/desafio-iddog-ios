6cc3a6b3a120d86784ecc9804ccc5ec15d390dfb
Carlos Candreva <carlosmc@ciandt.com>
2019-11-28 20:56:42 -0300

    Changing To Use punkapi

---
+++ desafiosIddog/Source/Model/Beer.swift
@@ -0,0 +1,29 @@
+//
+//  Beer.swift
+//  desafiosIddog
+//
+//  Created by Carlos Alberto Mota Candreva on 28/11/19.
+//  Copyright © 2019 Carlos Alberto Mota Candreva. All rights reserved.
+//
+
+import Foundation
+
+struct Beer: Decodable {
+    var id: Int?
+    var imageURL: String?
+    var name: String?
+    var tagline: String?
+    var alcoholByVolume: Double?
+    var internationalBitternessUnits: Double?
+    var description: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case imageURL = "image_url"
+        case name
+        case tagline
+        case alcoholByVolume = "abv"
+        case internationalBitternessUnits = "ibu"
+        case description
+    }
+}
